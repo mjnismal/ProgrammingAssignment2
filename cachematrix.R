203cf0464cfacb6a7251d457127502bd4de473a2
mjnismal <mjnismal@gmail.com>
2017-12-03 18:14:45 +0800

    erased some comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+
 ## Assumption: Matrix input is always invertible
 
-## Write a short comment describing this function
+
 
 
 ##This function creates a special "matrix" object that can cache its inverse.
